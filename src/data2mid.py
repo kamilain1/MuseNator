1491719f598d67338c861fd188acbc1c1d35052a
epetrashko <Lopory_324!>
2022-10-25 19:35:08 +0300

    Added functionality for converting data to mid and save it

--- src/data2mid.py
+++ src/data2mid.py
@@ -1,40 +1,40 @@
 import numpy as np
 import mido
 
-from const import NUMBER_OF_PIANO_NOTES
+from const import NUMBER_OF_PIANO_NOTES, FIRST_NOTE_ID
 
 
-def data2mid(ary, tempo=500000):
-    new_ary = np.concatenate([np.array([[0] * NUMBER_OF_PIANO_NOTES]), np.array(ary)], axis=0)
-    changes = new_ary[1:] - new_ary[:-1]
+def data2mid(data):
+    concatenated_data = np.concatenate([np.zeros((1, NUMBER_OF_PIANO_NOTES), dtype=np.int8), data])
+    changes = concatenated_data[1:] - concatenated_data[:-1]
+    # create empty mid
     mid_new = mido.MidiFile()
     track = mido.MidiTrack()
     mid_new.tracks.append(track)
-    track.append(mido.MetaMessage('set_tempo', tempo=tempo, time=0))
+
     last_time = 0
-    for ch in changes:
-        if set(ch) == {0}:
+    for change in changes:
+        # skip element if there are no changes
+        if all(change == 0):
             last_time += 1
         else:
-            on_notes = np.where(ch > 0)[0]
-            on_notes_vol = ch[on_notes]
-            off_notes = np.where(ch < 0)[0]
-            first_ = True
-            for n, v in zip(on_notes, on_notes_vol):
-                new_time = last_time if first_ else 0
-                track.append(mido.Message('note_on', note=n + 21, velocity=v, time=new_time))
-                first_ = False
-            for n in off_notes:
-                new_time = last_time if first_ else 0
-                track.append(mido.Message('note_off', note=n + 21, velocity=0, time=new_time))
-                first_ = False
+            on_notes = np.where(change > 0)[0]
+            off_notes = np.where(change < 0)[0]
+
+            # appending on_notes to the track
+            for index, note in enumerate(on_notes):
+                new_time = last_time if index == 0 else 0
+                track.append(
+                    mido.Message('note_on', note=note + FIRST_NOTE_ID, velocity=change[on_notes][0], time=new_time)
+                )
+            # appending off_notes to the track
+            for index, note in enumerate(off_notes):
+                new_time = last_time if index == 0 else 0
+                track.append(mido.Message('note_off', note=note + FIRST_NOTE_ID, velocity=0, time=new_time))
             last_time = 0
     return mid_new
 
 
-def save_midi(save_path, data):
-    mid_new = data2mid(data, 545455)
+def save_mid(save_path, data):
+    mid_new = data2mid(data)
     mid_new.save(f'{save_path}.mid')
-
-
-
--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 from preprocessing import get_piano_roll
-from data2mid import save_midi
+from data2mid import save_mid
 import mido
 
-if __name__ == '__main__':
-    datapath = "../dataset/A., Jag, Je t'aime Juliette, OXC7Fd0ZN8o.mid"
+
+def test_preprocessing(datapath: str, name: str = "../presentation/test"):
     mid = mido.MidiFile(datapath, clip=True)
     result_array = get_piano_roll(mid)
 
-    save_midi("../test", result_array)
+    save_mid(name, result_array)
+
+
+if __name__ == '__main__':
+    test_preprocessing(datapath="../dataset/A., Jag, Je t'aime Juliette, OXC7Fd0ZN8o.mid")
